edabf25674aa2f9920e99c1f762912ddded754bb
aep81 <aidan.power@duke.edu>
2024-10-03 19:43:10 -0400

    Task 6

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -26,7 +26,7 @@ date_dict = {}
 location_dict={}
 
 #Pretend we read one line of data from the file
-for lineString in line_list:
+for lineString in line_list: 
     #Check if line is a data line
     if lineString[0] in ("#","u"):
         continue 
@@ -41,9 +41,11 @@ for lineString in line_list:
     obs_lat = lineData[6]
     obs_lon = lineData[7]
 
-    #Add items to dictionaries
-    date_dict[record_id] = obs_date
-location_dict[record_id] = (obs_lat,obs_lon)
+    #Determine if location class criteria is met
+    if obs_lc in ("1","2","3"):
+        #Add items to dictionaries
+        date_dict[record_id] = obs_date
+        location_dict[record_id] = (obs_lat,obs_lon)
 
     #Print the location of sara
     #print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
